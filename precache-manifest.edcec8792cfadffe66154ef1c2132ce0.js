8f9f0ae2d5433c265b76eaa14e9a3d470b580a27
IvanSupervan <mogilevsky.ivan@gmail.com>
2020-06-01 15:43:36 +0300

    Updates

---
+++ precache-manifest.edcec8792cfadffe66154ef1c2132ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3db533997546c275f61c8045e4dfeee",
+    "url": "/rickopedia/index.html"
+  },
+  {
+    "revision": "5ffb360cf00d2b70b851",
+    "url": "/rickopedia/static/css/main.2a7de157.chunk.css"
+  },
+  {
+    "revision": "003c62a1daa53b2da85b",
+    "url": "/rickopedia/static/js/2.ebde0d2b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rickopedia/static/js/2.ebde0d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ffb360cf00d2b70b851",
+    "url": "/rickopedia/static/js/main.e7cd8907.chunk.js"
+  },
+  {
+    "revision": "20b8d7f28fe617c1dad6",
+    "url": "/rickopedia/static/js/runtime-main.afba7049.js"
+  }
+]);
